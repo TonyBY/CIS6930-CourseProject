200fdc55f7527e49e13117ae9e6fbde0c82e3cf9
Yang Bai <tony.bai.yang@gmail.com>
2020-11-04 18:33:43 -0500

    finished the baseline for the part 2.

---
+++ part2/src/colorization_utils.py
@@ -0,0 +1,116 @@
+import numpy as np
+import os, time
+from AverageMeter import AverageMeter
+import matplotlib.pyplot as plt
+import torch
+from skimage.color import lab2rgb
+
+
+# Move data into training and validation directories
+def split_data_set(data_path='../data/face_images/', size_of_validation_set=50):
+    os.makedirs(data_path + 'train/class/', exist_ok=True) # 700 images
+    os.makedirs(data_path + 'val/class/', exist_ok=True)   #  50 images
+    for i, file in enumerate(os.listdir(data_path)):
+        if "jpg" in file:
+            if i < size_of_validation_set: # first 50 will be val
+                os.rename(data_path + file, data_path + 'val/class/' + file)
+            else: # others will be train
+                os.rename(data_path + file, data_path + 'train/class/' + file)
+
+
+def to_rgb(grayscale_input, ab_input, save_path=None, save_name=None):
+    '''Show/save rgb image from grayscale and ab channels
+     Input save_path in the form {'grayscale': '/path/', 'colorized': '/path/'}'''
+    plt.clf() # clear matplotlib
+    color_image = torch.cat((grayscale_input, ab_input), 0).numpy() # combine channels
+    color_image = color_image.transpose((1, 2, 0))  # rescale for matplotlib
+    color_image[:, :, 0:1] = color_image[:, :, 0:1] * 100
+    color_image[:, :, 1:3] = color_image[:, :, 1:3] * 255 - 128
+    color_image = lab2rgb(color_image.astype(np.float64))
+    grayscale_input = grayscale_input.squeeze().numpy()
+    if save_path is not None and save_name is not None:
+        plt.imsave(arr=grayscale_input, fname='{}{}'.format(save_path['grayscale'], save_name), cmap='gray')
+        plt.imsave(arr=color_image, fname='{}{}'.format(save_path['colorized'], save_name))
+
+
+def train(train_loader, model, criterion, optimizer, epoch, use_gpu=False):
+    print('Starting training epoch {}'.format(epoch))
+    model.train()
+
+    # Prepare value counters and timers
+    batch_time, data_time, losses = AverageMeter(), AverageMeter(), AverageMeter()
+
+    end = time.time()
+    for i, (input_gray, input_ab, target) in enumerate(train_loader):
+        # Use GPU if available
+        if use_gpu: input_gray, input_ab, target = input_gray.cuda(), input_ab.cuda(), target.cuda()
+
+        # Record time to load data (above)
+        data_time.update(time.time() - end)
+
+        # Run forward pass
+        output_ab = model(input_gray)
+        loss = criterion(output_ab, input_ab)
+        losses.update(loss.item(), input_gray.size(0))
+
+        # Compute gradient and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Record time to do forward and backward passes
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # Print model accuracy -- in the code below, val refers to value, not validation
+        if i % 1 == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
+                  epoch, i, len(train_loader), batch_time=batch_time,
+                 data_time=data_time, loss=losses))
+
+    print('Finished training epoch {}'.format(epoch))
+
+
+def validate(val_loader, model, criterion, save_images, epoch, use_gpu=False):
+    model.eval()
+
+    # Prepare value counters and timers
+    batch_time, data_time, losses = AverageMeter(), AverageMeter(), AverageMeter()
+
+    end = time.time()
+    already_saved_images = False
+    for i, (input_gray, input_ab, target) in enumerate(val_loader):
+        data_time.update(time.time() - end)
+
+    # Use GPU
+    if use_gpu: input_gray, input_ab, target = input_gray.cuda(), input_ab.cuda(), target.cuda()
+
+    # Run model and record loss
+    output_ab = model(input_gray) # throw away class predictions
+    loss = criterion(output_ab, input_ab)
+    losses.update(loss.item(), input_gray.size(0))
+
+    # Save images to file
+    if save_images and not already_saved_images:
+        already_saved_images = True
+        for j in range(min(len(output_ab), 10)): # save at most 5 images
+            save_path = {'grayscale': '../data/colorization/outputs/gray/', 'colorized': '../data/colorization/outputs/color/'}
+            save_name = 'img-{}-epoch-{}.jpg'.format(i * val_loader.batch_size + j, epoch)
+            to_rgb(input_gray[j].cpu(), ab_input=output_ab[j].detach().cpu(), save_path=save_path, save_name=save_name)
+
+    # Record time to do forward passes and save images
+    batch_time.update(time.time() - end)
+    end = time.time()
+
+    # Print model accuracy -- in the code below, val refers to both value and validation
+    if i % 1 == 0:
+        print('Validate: [{0}/{1}]\t'
+            'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+            'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
+             i+1, len(val_loader), batch_time=batch_time, loss=losses))
+
+    print('Finished validation.')
+    return losses.avg
---
+++ part2/src/main.py
@@ -0,0 +1,119 @@
+import argparse
+import torch
+from torchvision import transforms
+from IPython.display import Image, display
+from os import path
+import torch.nn as nn
+import os
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+from ColorizationNet import ColorizationNet
+from GrayscaleImageFolder import GrayscaleImageFolder
+from colorization_utils import train, validate, split_data_set
+
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(
+        description='A CNN based image colorizer.'
+    )
+    parser.add_argument('-data_path', default='../data/face_images/',
+                        help='data path of file containing the face images')
+    parser.add_argument('-model_path', default='../data/colorization/checkpoints/model-epoch-1-losses-0.004.pth/',
+                        help='Pre-trained model path of ColorizationNet')
+    parser.add_argument('-save_images', action='store_true',
+                        help='Cache grey image and colorized images of validation set.')
+    parser.add_argument('-split_dataset', action='store_true',
+                        help='Split the dataset into training set and validation set.')
+    parser.add_argument('-validation_size', default='50', help='the number of images in the validation set.')
+    parser.add_argument('-mode', default='eval', help='Option: train/eval')
+    parser.add_argument('-use_gpu', action='store_true',
+                        help='Use gup if available.')
+    return parser.parse_args(args)
+
+
+def main(args):
+    # Check if GPU is available
+    use_gpu = torch.cuda.is_available() and args.use_gpu
+
+    data_path = args.data_path
+    if args.split_dataset and path.exists(data_path + "image00000.jpg"):
+        split_data_set(data_path=data_path, size_of_validation_set=int(args.validation_size))
+
+
+    # Make sure the images are there
+    display(Image(filename=data_path + 'train/class/image00000.jpg'))
+
+    # Training
+    train_transforms = transforms.Compose([transforms.RandomResizedCrop(224), transforms.RandomHorizontalFlip()])
+    train_imagefolder = GrayscaleImageFolder(data_path + 'train', train_transforms)
+    train_loader = torch.utils.data.DataLoader(train_imagefolder, batch_size=64, shuffle=True)
+
+    # Validation
+    val_transforms = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224)])
+    val_imagefolder = GrayscaleImageFolder(data_path + 'val', val_transforms)
+    val_loader = torch.utils.data.DataLoader(val_imagefolder, batch_size=64, shuffle=False)
+
+    model = ColorizationNet()
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=0.0)
+
+    save_images = args.save_images
+
+    if args.mode == 'eval':
+        # Load model
+        pretrained = torch.load(
+            args.model_path,
+            map_location=lambda storage, loc: storage)
+
+
+        model.load_state_dict(pretrained)
+
+        # Validate
+        with torch.no_grad():
+            validate(val_loader, model, criterion, save_images, 0, use_gpu=use_gpu)
+    elif args.mode == 'train':
+        if use_gpu:
+            criterion = criterion.cuda()
+            model = model.cuda()
+
+        # Make folders and set parameters
+        os.makedirs('../data/colorization/outputs/color', exist_ok=True)
+        os.makedirs('../data/colorization/outputs/gray', exist_ok=True)
+        os.makedirs('../data/colorization/checkpoints', exist_ok=True)
+        best_losses = 1e10
+        epochs = 100
+
+        # Train model
+        for epoch in range(epochs):
+            # Train for one epoch, then validate
+            train(train_loader, model, criterion, optimizer, epoch)
+            with torch.no_grad():
+                losses = validate(val_loader, model, criterion, save_images, epoch)
+            # Save checkpoint and replace old best model if current model is better
+            if losses < best_losses:
+                best_losses = losses
+                torch.save(model.state_dict(),
+                           '../data/colorization/checkpoints/model-epoch-{}-losses-{:.3f}.pth'.format(epoch + 1, losses))
+    elif args.mode == 'demo':
+        # Show images
+        image_pairs = [
+            ('../data/colorization/outputs/color/img-0-epoch-0.jpg',
+             '../data/colorization/outputs/gray/img-0-epoch-0.jpg'),
+            ('../data/colorization/outputs/color/img-1-epoch-0.jpg',
+             '../data/colorization/outputs/gray/img-1-epoch-0.jpg')]
+        for c, g in image_pairs:
+            color = mpimg.imread(c)
+            gray = mpimg.imread(g)
+            f, axarr = plt.subplots(1, 2)
+            f.set_size_inches(15, 15)
+            axarr[0].imshow(gray, cmap='gray')
+            axarr[1].imshow(color)
+            axarr[0].axis('off'), axarr[1].axis('off')
+            plt.show()
+    else:
+        raise ValueError('mode can only be demo, eval or train.')
+
+
+if __name__ == '__main__':
+    main(parse_args())
