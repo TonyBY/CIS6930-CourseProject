00a6dc30926b7f1e7c4776f9a35b17221cfe9df2
TonyBY <tony.bai.yang@gmail.com>
2020-11-18 12:33:27 -0500

    Added code to evaluate yolo mask detecter with videos.

--- part3/src/yolov3/YoLoV3MaskDetection.py
+++ part3/src/yolov3/YoLoV3MaskDetection.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def main():
-    net = cv2.dnn.readNet('yolov3_training_last.weights', 'yolov3_testing.cfg')
+    net = cv2.dnn.readNet('../../data/data2/YoLoV3/yolov3_training_last.weights', 'yolov3_testing.cfg')
 
     classes = []
     with open("classes.txt", "r") as f:
@@ -13,52 +13,71 @@ def main():
     font = cv2.FONT_HERSHEY_PLAIN
     colors = np.random.uniform(0, 255, size=(100, 3))
 
+    img_array = []
+    img_idx = 0
+    frame_idx = 0
     while True:
-        _, img = cap.read()
-        height, width, _ = img.shape
+        img_idx += 1
+        try:
+            _, img = cap.read()
+            if img_idx % 24 == 0:
+                frame_idx += 1
+                print("##############################################")
+                print("Processing frame: %s (24 images/frame)" % frame_idx)
+            height, width, _ = img.shape
 
-        blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0,0,0), swapRB=True, crop=False)
-        net.setInput(blob)
-        output_layers_names = net.getUnconnectedOutLayersNames()
-        layerOutputs = net.forward(output_layers_names)
+            blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0,0,0), swapRB=True, crop=False)
+            net.setInput(blob)
+            output_layers_names = net.getUnconnectedOutLayersNames()
+            layerOutputs = net.forward(output_layers_names)
 
-        boxes = []
-        confidences = []
-        class_ids = []
+            boxes = []
+            confidences = []
+            class_ids = []
 
-        for output in layerOutputs:
-            for detection in output:
-                scores = detection[5:]
-                class_id = np.argmax(scores)
-                confidence = scores[class_id]
-                if confidence > 0.2:
-                    center_x = int(detection[0]*width)
-                    center_y = int(detection[1]*height)
-                    w = int(detection[2]*width)
-                    h = int(detection[3]*height)
+            for output in layerOutputs:
+                for detection in output:
+                    scores = detection[5:]
+                    class_id = np.argmax(scores)
+                    confidence = scores[class_id]
+                    if confidence > 0.2:
+                        center_x = int(detection[0]*width)
+                        center_y = int(detection[1]*height)
+                        w = int(detection[2]*width)
+                        h = int(detection[3]*height)
 
-                    x = int(center_x - w/2)
-                    y = int(center_y - h/2)
+                        x = int(center_x - w/2)
+                        y = int(center_y - h/2)
 
-                    boxes.append([x, y, w, h])
-                    confidences.append((float(confidence)))
-                    class_ids.append(class_id)
+                        boxes.append([x, y, w, h])
+                        confidences.append((float(confidence)))
+                        class_ids.append(class_id)
 
-        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.2, 0.4)
+            indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.2, 0.4)
 
-        if len(indexes) > 0:
-            for i in indexes.flatten():
-                x, y, w, h = boxes[i]
-                label = str(classes[class_ids[i]])
-                confidence = str(round(confidences[i],2))
-                color = colors[i]
-                cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
-                cv2.putText(img, label + " " + confidence, (x, y+20), font, 2, (255,255,255), 2)
+            if len(indexes) > 0:
+                for i in indexes.flatten():
+                    x, y, w, h = boxes[i]
+                    label = str(classes[class_ids[i]])
+                    confidence = str(round(confidences[i],2))
+                    color = colors[i]
+                    cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+                    cv2.putText(img, label + " " + confidence, (x, y+20), font, 2, (255,255,255), 2)
 
-        cv2.imshow('Image', img)
-        key = cv2.waitKey(1)
-        if key == 27:
+            img_array.append(img)
+            # cv2.imshow('Image', img)
+            # key = cv2.waitKey(1)
+            # if key == 27:
+            #     break
+        except AttributeError:
             break
+
+    print("Generating the video with the processed images.")
+    out = cv2.VideoWriter('../../data/data2/testing/videos/mask_detection_out.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 20.0, (width, height))
+    for i in range(len(img_array)):
+        out.write(img_array[i])
+    out.release()
+    print("Done")
 
     cap.release()
     cv2.destroyAllWindows()
