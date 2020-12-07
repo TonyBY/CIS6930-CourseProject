ffa3c15379f870919b68de74e7e5e46212532eeb
y w <yw@ydeMacBook-Pro.local>
2020-12-07 00:53:06 -0500

    cnn baseline

---
+++ part3/src/cnn/CNNDataset.py
@@ -0,0 +1,69 @@
+from PIL import Image
+import os
+import numpy as np
+import cv2
+import torch
+
+from CNN_utils import generate_target
+
+class MaskDataset(object):
+    def __init__(self, transforms, imgs_path, labels_path, mode):
+        self.transforms = transforms
+        # load all image files, sorting them to ensure that they are aligned
+        self.imgs = list(sorted(os.listdir(imgs_path)))
+        self.imgs_path = imgs_path
+        self.labels_path = labels_path
+        self.mode = mode
+
+    def __getitem__(self, idx):
+        if self.mode == "eval":
+            idx = idx + 842
+
+        # load images ad masks
+        file_image = 'maksssksksss' + str(idx) + '.png'
+        file_label = 'maksssksksss' + str(idx) + '.xml'
+        img_path = os.path.join(self.imgs_path, file_image)
+        label_path = os.path.join(self.labels_path, file_label)
+        img = Image.open(img_path).convert("RGB")
+        if self.transforms is not None:
+            img = self.transforms(img)
+
+        #resize image
+        arr_img = np.asarray(img).transpose((1, 2, 0))
+        # print(idx, 'before Dimensions : ',arr_img.shape)
+        width = 300
+        height = 300
+        dim = (width, height)
+        arr_img = cv2.resize(arr_img, dim, interpolation = cv2.INTER_AREA)
+        # print('Resized Dimensions : ',arr_img.shape)
+        arr_img = torch.from_numpy(arr_img.transpose((2, 0, 1))).float()
+        
+        # Generate normalized labels
+        width_scale = width / img.shape[2]
+        height_scale = height / img.shape[1] 
+        # print("scale:",width_scale,height_scale)
+        target = generate_target(idx, label_path, width_scale,height_scale)
+        # print(arr_img.size())
+        return arr_img, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+# transform annotations into tensor(4*4*144*144)
+def create_window_label(annotations):
+    result = torch.zeros(len(annotations),4,144,144)
+    scale = 144/300
+    for i in range(len(annotations)): #each image
+        #initial class0 = 1, background
+        result[i,0] = torch.ones(144,144)
+        # print(annotations[i]['labels'])
+        for j in range(len(annotations[i]['labels'])):
+            label = annotations[i]['labels'][j]
+            [xmin, ymin, xmax, ymax] = annotations[i]['boxes'][j]
+            xmin, ymin, xmax, ymax = int(xmin*scale), int(ymin*scale), min(int(xmax*scale),143), min(int(ymax*scale),143)
+            bb = torch.ones(ymax+1-ymin, xmax+1-xmin)
+            bg = torch.zeros(ymax+1-ymin, xmax+1-xmin)
+            # print(bb.size(),result[i, label, ymin:ymax+1, xmin:xmax+1].size())
+            result[i, label, ymin:ymax+1, xmin:xmax+1] = bb
+            result[i,0, ymin:ymax+1, xmin:xmax+1] = bg
+    return result
---
+++ part3/src/cnn/CNN_main.py
@@ -0,0 +1,110 @@
+import torch
+# import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+# import matplotlib.pyplot as plt
+import os
+import argparse
+import torch.nn as nn
+import numpy as np
+import sys
+
+
+from CNNNet import *
+from CNNDataset import MaskDataset
+from CNN_utils import *
+
+np.set_printoptions(threshold=sys.maxsize)
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+def main(args):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
+    if args.mode == 'train':
+        imgs_path = args.data_path + "training/images/"
+        labels_path = args.data_path + "training/annotations/"
+    elif args.mode == "eval":
+        imgs_path = args.data_path + "testing/images/"
+        labels_path = args.data_path + "testing/annotations/"
+    
+    data_transform = transforms.Compose([transforms.ToTensor()])
+    dataset = MaskDataset(data_transform, imgs_path, labels_path, args.mode)
+    if args.mode == 'train':
+        trainloader = torch.utils.data.DataLoader(dataset, batch_size=4, collate_fn=collate_fn, shuffle=False)
+        net = CnnNet()
+        net.to(device)
+        # Loss function and optimizer
+        # parameters
+        params = [p for p in net.parameters() if p.requires_grad]
+        criterion = nn.BCELoss()
+        optimizer = torch.optim.SGD(params, lr=0.001,
+                                    momentum=0.9, weight_decay=0.0005)
+        len_dataloader = len(trainloader)
+
+        # Make folders and set parameters
+        os.makedirs('../data/data2/FasterRCNN/outputs', exist_ok=True)
+        os.makedirs('../data/data2/FasterRCNN/checkpoints', exist_ok=True)
+        best_losses = 0.001
+
+        #train the model
+        CNN_taining(trainloader, net, device, criterion, optimizer)
+        
+        # save trained model
+        PATH = 'cnn_net.pth'
+        torch.save(net.state_dict(), PATH)
+    
+    elif args.mode == 'eval':
+        testloader = torch.utils.data.DataLoader(dataset, batch_size=1, collate_fn=collate_fn, shuffle=False)
+        # load the trained model
+        net = CnnNet()
+        net.load_state_dict(torch.load(PATH))
+
+        # network performs on the whole dataset.
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data
+                outputs = net(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        print('Accuracy of the network on the 10000 test images: %d %%' % (
+            100 * correct / total))
+
+        #  the classes that performed well, and the classes that did not perform well
+        # class_correct = list(0. for i in range(10))
+        # class_total = list(0. for i in range(10))
+        # with torch.no_grad():
+        #     for data in testloader:
+        #         images, labels = data
+        #         outputs = net(images)
+        #         _, predicted = torch.max(outputs, 1)
+        #         c = (predicted == labels).squeeze()
+        #         for i in range(4):
+        #             label = labels[i]
+        #             class_correct[label] += c[i].item()
+        #             class_total[label] += 1
+
+        # for i in range(10):
+        #     print('Accuracy of %5s : %2d %%' % (
+        #         classes[i], 100 * class_correct[i] / class_total[i]))
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(
+        description='A CNN based image colorizer.'
+    )
+    parser.add_argument('-data_path', default='./data/data2/',
+                        help='data path of file containing the data')
+    # parser.add_argument('-labels_path', default='../data/data2/annotations/',
+    #                     help='data path of file containing the annotations')
+    parser.add_argument('-model_path', default='./data/data2/FasterRCNN/checkpoints/model-epoch-253-losses-0.0000.pth',
+                        help='Pre-trained model path of FasterRCNN')
+    parser.add_argument('-mode', default='train', help='Option: train/eval')
+
+    return parser.parse_args(args)
+
+if __name__ == '__main__':
+    main(parse_args())
